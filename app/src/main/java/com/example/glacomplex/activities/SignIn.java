458c8910516bcd7e8e0e602d364afb2594329a6c
Sanjeev <sanjeev718191@gmail.com>
2022-11-20 02:12:12 +0530

    Commit (20-11-22)- User Login and SignUp Implemented

--- app/src/main/java/com/example/glacomplex/activities/SignIn.java
+++ app/src/main/java/com/example/glacomplex/activities/SignIn.java
@@ -1,24 +1,41 @@
 package com.example.glacomplex.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.glacomplex.R;
 import com.example.glacomplex.databinding.ActivitySignInBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignIn extends AppCompatActivity {
 
     ActivitySignInBinding binding;
+    String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+    ProgressDialog progressDialog;
+
+    FirebaseAuth mAuth;
+    FirebaseUser mUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivitySignInBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        progressDialog = new ProgressDialog(this);
+        mAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
 
         binding.signUp.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,5 +44,52 @@ public class SignIn extends AppCompatActivity {
             }
         });
 
+        binding.signInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                performLogin();
+            }
+        });
+
     }
+    private void performLogin(){
+        String email = binding.userMail.getText().toString();
+        String password = binding.password.getText().toString();
+
+        if(!email.matches(emailPattern)){
+            binding.userMail.setError("Enter Correct Email");
+            binding.userMail.requestFocus();
+        }else if(password.isEmpty() || password.length() < 6) {
+            binding.password.setError("Enter Proper Password");
+        } else {
+            progressDialog.setMessage("Please wait while Login...");
+            progressDialog.setTitle("LogIn");
+            progressDialog.setCanceledOnTouchOutside(false);
+            progressDialog.show();
+
+            mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()){
+                        progressDialog.dismiss();
+                        //add true in sharedpreference
+                        Toast.makeText(SignIn.this, "LogIn Sucessfull", Toast.LENGTH_SHORT).show();
+                        sendToMain();
+                    } else {
+                        progressDialog.dismiss();
+                        //add false in sharedpreference
+                        Toast.makeText(SignIn.this, task.getException().toString(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+
+        }
+    }
+
+    private void sendToMain(){
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+
 }
--- app/src/main/java/com/example/glacomplex/activities/SignUp.java
+++ app/src/main/java/com/example/glacomplex/activities/SignUp.java
@@ -1,21 +1,44 @@
 package com.example.glacomplex.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.glacomplex.databinding.ActivitySignUpBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class SignUp extends AppCompatActivity {
 
     ActivitySignUpBinding binding;
+    String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+    ProgressDialog progressDialog;
+
+    FirebaseAuth mAuth;
+    FirebaseUser mUser;
+
+    SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
          binding = ActivitySignUpBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        progressDialog = new ProgressDialog(this);
+        mAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
+        //sharedPreferences = getSharedPreferences();
 
         binding.signInBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,5 +47,58 @@ public class SignUp extends AppCompatActivity {
             }
         });
 
+        binding.signUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                performAuth();
+            }
+        });
+
     }
+
+    private void performAuth(){
+        String email = binding.userMail.getText().toString();
+        String password = binding.password.getText().toString();
+        String confirmPassword = binding.confirmPassword.getText().toString();
+        if(!email.matches(emailPattern)){
+            binding.userMail.setError("Enter Correct Email");
+            binding.userMail.requestFocus();
+        }else if(password.isEmpty() || password.length() < 6) {
+            binding.password.setError("Enter Proper Password");
+        } else if(confirmPassword.isEmpty()){
+            binding.confirmPassword.setError("Required");
+        } else if(!password.equals(confirmPassword)){
+            binding.confirmPassword.setError("Password Not Mached");
+        } else {
+            progressDialog.setMessage("Please wait while Registration...");
+            progressDialog.setTitle("Registration");
+            progressDialog.setCanceledOnTouchOutside(false);
+            progressDialog.show();
+
+            mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()){
+                        progressDialog.dismiss();
+                       //add true in sharedpreference
+                        Toast.makeText(SignUp.this, "Registaration Successfull", Toast.LENGTH_SHORT).show();
+                        sendToMain();
+                    } else {
+                        progressDialog.dismiss();
+                        //add false in sharedpreference
+                        Toast.makeText(SignUp.this, task.getException().toString(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+
+        }
+        
+    }
+
+    private void sendToMain(){
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+
 }
